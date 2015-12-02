c2f512ace30d5396a81f864197bd60adcb12c778
areilly <areilly@tycoint.com>
2015-12-02 13:04:35 +0000

    tweaks

--- generate_topics/list_2_topics.bat
+++ generate_topics/list_2_topics.bat
@@ -6,7 +6,7 @@ for /f "tokens=*" %%a in (topics.txt) do (type nul>"%%a.dita")
 for /f "tokens=*" %%a in (topics.txt) do (
   echo ^<?xml version="1.0" encoding="UTF-8"?^> >>"%%a.dita"
   echo ^<!DOCTYPE concept PUBLIC "-//OASIS//DTD DITA Concept//EN" "concept.dtd"^> >>"%%a.dita"
-  echo ^<concept id="%%a"^> >>"%%a.dita"
+  echo ^<concept id="con_%%a"^> >>"%%a.dita"
   echo ^<title^>^</title^> >>"%%a.dita"
   echo ^<shortdesc^>^</shortdesc^> >>"%%a.dita"
   echo ^<conbody^> >>"%%a.dita"
